bddbad950903bc474b438dfcb1e4f1e5b59376e1
patrick <patrick_donovan@outlook.com>
2022-09-02 17:01:11 +1000

    idk

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 700,
+    minHeight: 700,
+    minWidth: 400,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
