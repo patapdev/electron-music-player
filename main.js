29bdb47bb93303a544d047a35bb79e148e4003ed
patrick <patrick_donovan@outlook.com>
2022-09-02 15:18:43 +1000

    init commit

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ function createWindow () {
       preload: path.join(__dirname, 'preload.js')
     }
   })
+  mainWindow.setMenuBarVisibility(false)
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
